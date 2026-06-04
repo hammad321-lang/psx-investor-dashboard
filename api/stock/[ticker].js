b5d43184e91272ace7fe78441955fa981606b7aa
hammad321-lang <hammad321@gmail.com>
2026-06-04 23:09:07 +0500

    Add shariah compliance and dividends to stock data

--- api/stock/[ticker].js
+++ api/stock/[ticker].js
@@ -1,33 +1,29 @@
 export default function handler(req, res) {
+
     const { ticker } = req.query;
 
     const db = {
-        SYS: { price: 438, bv: 114.4, shares: 291, sector: "Tech" },
-        FFC: { price: 195, bv: 110.2, shares: 1272, sector: "Fertilizer" },
-        EFERT: { price: 168.4, bv: 58.2, shares: 1335, sector: "Fertilizer" },
-        MARI: { price: 2450, bv: 980.5, shares: 133, sector: "Oil & Gas" },
-        OGDC: { price: 122.3, bv: 265.4, shares: 4301, sector: "Oil & Gas" }
+        SYS: {
+            price: 438,
+            bv: 114.4,
+            shares: 291,
+            sector: "Tech",
+            shariah: "YES",
+            divs: [8, 6, 5, 4, 3]
+        }
     };
 
-    const data = db[ticker.toUpperCase()];
+    const stock = db[ticker];
 
-    if (!data) {
-        return res.json({
-            ticker,
-            price: 100,
-            bv: 100,
-            shares: 100,
-            sector: "Unknown"
+    if (!stock) {
+        return res.status(404).json({
+            error: "Ticker not found",
+            ticker
         });
     }
 
-    return res.json({
+    return res.status(200).json({
         ticker,
-        price: data.price,
-        bv: data.bv,
-        shares: data.shares,
-        sector: data.sector,
-        shariah: "YES",
-        dividends: [0,0,0,0,0]
+        ...stock
     });
 }
